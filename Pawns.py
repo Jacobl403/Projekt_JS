54e584a2029bc57dd1923fbfe1acef3e1d068355
Jacobl403 <64545058+Jacobl403@users.noreply.github.com>
2020-06-18 05:54:58 +0200

    s

---
+++ Pawns.py
@@ -0,0 +1,49 @@
+
+
+
+class pawn(object):
+    def __init__(self, x, y, type_piece):
+        self.x = x
+        self.y = y
+        self.type_piece = type_piece
+
+    def list_of_moves(self):
+        move_list=[]
+        if self.type_piece=='B':
+            move_list.append((self.x+1,self.y+1))
+            move_list.append((self.x+1,self.y-1))
+        if self.type_piece=='C':
+            move_list.append((self.x-1,self.y+1))
+            move_list.append((self.x-1,self.y-1))
+        return move_list
+    def list_of_jumps(self):
+        jump_list=[]
+        if self.type_piece=='B':
+            jump_list.append((self.x+2,self.y+2))
+            jump_list.append((self.x+2,self.y-2))
+        if self.type_piece=='C':
+            jump_list.append((self.x-2,self.y+2))
+            jump_list.append((self.x-2,self.y-2))
+        return jump_list
+
+
+class Quin(pawn):
+    def __init__(self,x,y,type_piece):
+        super().__init__(x,y,type_piece)
+    def list_of_moves(self):
+        move_list=[]
+        move_list.append((self.x+1,self.y+1))
+        move_list.append((self.x+1,self.y-1))
+        move_list.append((self.x-1,self.y+1))
+        move_list.append((self.x-1,self.y-1))
+        return move_list
+
+    def list_of_jumps(self):
+        jump_list=[]
+        jump_list.append((self.x + 2, self.y + 2))
+        jump_list.append((self.x + 2, self.y - 2))
+        jump_list.append((self.x - 2, self.y + 2))
+        jump_list.append((self.x - 2, self.y - 2))
+        return jump_list
+
+
---
+++ main.py
@@ -0,0 +1,70 @@
+import pygame
+import Game_Logic
+import os
+from math import sqrt
+def init_screen():
+    Window_x = 300
+    Window_y = 100
+    os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (Window_x, Window_y)
+    pygame.init()
+    pygame.display.set_caption("English Checkers")
+    screen = pygame.display.set_mode((600, 540))
+    # icon = pygame.image.load('game.png')
+    # pygame.display.set_icon(icon)
+    return screen
+
+def pawn_display(screen, type_pawn,center):
+    red=(255,0,0)
+    color_p = (255, 223, 24)
+    white=(255,255,255)
+    distance=55
+    if type_pawn=='C':
+         pygame.draw.circle(screen, red, center, 25)
+    elif type_pawn == 'B':
+        pygame.draw.circle(screen, color_p, center, 25)
+    elif type_pawn=='Cd':
+         pygame.draw.circle(screen, red, center, 25)
+         pygame.draw.circle(screen, white, center, 27,1)
+    elif type_pawn == 'Bd':
+        pygame.draw.circle(screen, color_p, center, 25)
+        pygame.draw.circle(screen, white, center, 28,1)
+
+#todo tu zszytuje za kazdym razem stan boardu
+def DisplayBoard(screen,grid):
+    black = (0, 0, 0)
+    white = (255, 255, 255)
+    wood = (166, 128, 100)
+    x,y=15,35
+    size=60
+    screen.fill(wood)
+    for row in range(8):
+        for column in range(8):
+            if (row%2==0 and column%2==0) or (row % 2 != 0 and column % 2 != 0):
+                pygame.draw.rect(screen, white, (x + (row * size), y + (column * size), size, size))
+            if row % 2 != 0 and column % 2 == 0 or (row % 2 == 0 and column % 2 != 0):
+                block=pygame.draw.rect(screen, black, (x + (row * size), y + (column * size), size,size))
+                pawn_display(screen, grid.board[column][row], block.center)
+
+
+
+def main():
+    screen=init_screen()
+    grid=Game_Logic.Game_Logic()
+    grid.test()
+    running = True
+
+    while running:
+        screen.fill((0,0,0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        mouse_click=pygame.mouse.get_pressed()
+        if mouse_click[0]:
+            x,y=pygame.mouse.get_pos()
+
+
+        DisplayBoard(screen, grid)
+        pygame.display.update()
+if __name__ == "__main__":
+    main()
+
